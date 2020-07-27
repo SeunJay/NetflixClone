fd88fc1ed7c6d4df4b2cdbc3fd931b3c6173d229
seun jay <seunjay92@gmail.com>
2020-07-27 18:11:18 +0100

    create Jumbotron container

---
+++ src/containers/jumbotron.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Jumbotron from "../components/jumbotron";
+
+export const JumbotronContainer = () => {
+  return (
+    <Jumbotron.Container>
+      <Jumbotron.Title>I am home</Jumbotron.Title>
+      <Jumbotron.SubTitle>I am home</Jumbotron.SubTitle>
+    </Jumbotron.Container>
+  );
+};
