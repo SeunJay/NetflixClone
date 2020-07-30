1d46172043592236cd5e7d222f379e608ef0524e
seun jay <seunjay92@gmail.com>
2020-07-30 10:52:13 +0100

    import fixtures data

--- src/containers/jumbotron.js
+++ src/containers/jumbotron.js
@@ -1,11 +1,21 @@
 import React from "react";
 import Jumbotron from "../components/jumbotron";
+import jumboData from "../fixtures/jumbo";
 
 export const JumbotronContainer = () => {
   return (
     <Jumbotron.Container>
-      <Jumbotron.Title>I am home</Jumbotron.Title>
-      <Jumbotron.SubTitle>I am home</Jumbotron.SubTitle>
+      {jumboData.map((item) => (
+        <Jumbotron key={item.id} direction={item.direction}>
+          <Jumbotron.Pane>
+            <Jumbotron.Title>{item.title}</Jumbotron.Title>
+            <Jumbotron.SubTitle>{item.subTitle}</Jumbotron.SubTitle>
+          </Jumbotron.Pane>
+          <Jumbotron.Pane>
+            <Jumbotron.Image src={item.image} alt={item.alt} />
+          </Jumbotron.Pane>
+        </Jumbotron>
+      ))}
     </Jumbotron.Container>
   );
 };
