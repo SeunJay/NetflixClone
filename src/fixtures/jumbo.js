e290b64ecf945675337033c81b4b7586ccdaffbd
seun jay <seunjay92@gmail.com>
2020-07-30 10:50:25 +0100

    convert fixture json file to js file

---
+++ src/fixtures/jumbo.js
@@ -0,0 +1,30 @@
+const data = [
+  {
+    id: 1,
+    title: "Enjoy on your TV.",
+    subTitle:
+      "Watch on smart TVs, PlayStation, Xbox, Chromecast, Apple TV, Blu-ray players and more.",
+    image: require("../images/misc/home-tv.jpg"),
+    alt: "Tiger King on Netflix",
+    direction: "row",
+  },
+  {
+    id: 2,
+    title: "Download your programmes to watch on the go.",
+    subTitle: "Save your data and watch all your favourites offline.",
+    image: require("../images/misc/home-mobile.jpg"),
+    alt: "Tiger King on Netflix",
+    direction: "row-reverse",
+  },
+  {
+    id: 3,
+    title: "Watch everywhere.",
+    subTitle:
+      "Stream unlimited films and TV programmes on your phone, tablet, laptop and TV without paying more.",
+    image: require("../images/misc/home-imac.jpg"),
+    alt: "Money Heist on Netflix",
+    direction: "row",
+  },
+];
+
+export default data
