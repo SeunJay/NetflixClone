69153e43df68664b34dd9c9b7741eebfd942576b
seun jay <seunjay92@gmail.com>
2020-08-09 14:49:14 +0100

    add header container to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,10 +2,12 @@ import React from "react";
 import { JumbotronContainer } from "../containers/jumbotron";
 import { FootContainer } from "../containers/footer";
 import { FaqsContainer } from "../containers/accordion";
+import { HeaderContainer } from "../containers/header";
 
 const Home = () => {
   return (
     <>
+      <HeaderContainer />
       <JumbotronContainer />
       <FaqsContainer />
       <FootContainer />
